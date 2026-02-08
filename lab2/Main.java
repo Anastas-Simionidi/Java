e6081611946250d879db99199bb36c3a1d354796
Anastas-Simionidi <anastassimionidi@gmail.com>
2026-02-08 20:52:57 +0500

    Создал паку lab2

---
+++ lab2/Main.java
@@ -0,0 +1,21 @@
+package lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Переменные примитивных типов
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent = true;
+
+        // Константа (final)
+        final double PI = 3.14159;
+
+        // Вывод значений в консоль
+        System.out.println("Возраст: " + age + " лет");
+        System.out.println("Рост: " + height + " м");
+        System.out.println("Является ли студентом: " + isStudent);
+        System.out.println("Константа PI: " + PI);
+    }
+}
